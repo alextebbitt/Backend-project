c9b162a427934d0115c2649762ebdc07374240a7
alextebbitt <alex.tebbitt87@gmail.com>
2022-05-31 13:46:25 +0200

    routes

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT = 8787;
+
+const { dbConnection } = require("./config/config");
+app.use(express.json());
+
+dbConnection();
+app.use("/posts", require("./routes/posts"));
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
