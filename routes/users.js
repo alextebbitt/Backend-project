9c3c285219f3d7a0ea7bea42d48893ec0c21e971
alextebbitt <alex.tebbitt87@gmail.com>
2022-06-01 17:44:15 +0200

    please confirm your email

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ router.get("/search/:name", UserController.getUsersByName);
 router.delete("/id/:_id", authentication, isAdmin, UserController.delete);
 router.put("/", authentication, UserController.update);
 router.get("/confirm/:emailToken", UserController.confirm);
-router.post("/login", UserController.login);
+router.post("/login",  UserController.login);
 router.put("/logout", authentication, UserController.logout);
 
 module.exports = router;
